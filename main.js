5fad7a366afb5d34768360bd3d83aff5b0bf206b
Hubert Muller <hubert@sportport.eu>
2022-02-26 12:42:18 +0100

    zasiegi

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+// komentarz jednoliniowy
+/*
+wieloniowy
+komentarz
+*/
+
+let powiekszenie = 0;
+//var x = 5;
+
+function zmien() {
+    //var x;
+    //x = 64;
+    var x = 34;
+    console.log(x);
+    if (powiekszenie == 0) {
+        powiekszenie = 1;
+    } else {
+        powiekszenie = 0;
+    }
+    
+}
+
+function p () {
+    x = 66;
+    console.log(x);
+}
+
 console.log("Czesc");
+
+zmien();
+zmien();
+zmien();
+p();
