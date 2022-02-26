c27f68dfa43135f1be7c66193057dc97baf79716
Hubert Muller <hubert@sportport.eu>
2022-02-26 13:08:32 +0100

    powiekszanie

--- main.js
+++ main.js
@@ -4,30 +4,7 @@ wieloniowy
 komentarz
 */
 
-let powiekszenie = 0;
-//var x = 5;
-
-function zmien() {
-    //var x;
-    //x = 64;
-    var x = 34;
-    console.log(x);
-    if (powiekszenie == 0) {
-        powiekszenie = 1;
-    } else {
-        powiekszenie = 0;
-    }
-    
+function zmien(rozmiar) {
+    var el = document.querySelector('html');
+    el.style.fontSize = rozmiar;
 }
-
-function p () {
-    x = 66;
-    console.log(x);
-}
-
-console.log("Czesc");
-
-zmien();
-zmien();
-zmien();
-p();
